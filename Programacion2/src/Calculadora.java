e7d7cce35a42922f3124080f9757d71424c7af8a
Rocio Pacheco Ponce <rociopa1995@gmail.com>
2024-12-18 15:12:29 +0100

    subido

--- Programacion2/src/Calculadora.java
+++ Programacion2/src/Calculadora.java
@@ -1,5 +1,10 @@
 
 public class Calculadora {
+	private static final String DIV = "DIV";
+	private static final String MUL = "MUL";
+	private static final String SUB = "SUB";
+	private static final String ADD = "ADD";
+
 	public static Integer sumar(Integer numero, Integer numero1) {
 		Integer suma = numero + numero1;
 		return suma;
@@ -21,5 +26,22 @@ public class Calculadora {
 		}
 		Integer divi = numero / numero1;
 		return divi;
+	}
+
+	public static Integer calcular(String cadena, Integer numero, Integer numero1) {
+		if (cadena.equalsIgnoreCase(ADD)) {
+			return Calculadora.sumar(numero, numero1);
+		}
+		if (cadena.equalsIgnoreCase(SUB)) {
+			return Calculadora.restar(numero, numero1);
+		}
+		if (cadena.equalsIgnoreCase(MUL)) {
+			return Calculadora.multiplicar(numero, numero1);
+		}
+		if (cadena.equalsIgnoreCase(DIV)) { // para sacar la constante click derecho-refactor-extraer constante
+			return Calculadora.dividir(numero, numero1);
+		}
+		// siempre te pide un return si no entra en los demás if
+		return null;
 	}
 }
--- Programacion2/src/Metodos6.java
+++ Programacion2/src/Metodos6.java
@@ -3,18 +3,14 @@ import java.util.Scanner;
 public class Metodos6 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce la operación ADD, SUB, MUL O DIV: ");
+		String cadena = sc.nextLine();
 		System.out.println("Introduce un numero");
 		Integer numero = sc.nextInt();
 		System.out.println("Introduce otro numero");
 		Integer numero1 = sc.nextInt();
-		Integer sumaHecha = Calculadora.sumar(numero, numero1);
-		System.out.println("La suma es: " + sumaHecha);
-		Integer resta = Calculadora.restar(numero, numero1);
-		System.out.println("la resta es:" + resta);
-		Integer multi = Calculadora.multiplicar(numero, numero1);
-		System.out.println("la multiplicación es" + multi);
-		Integer divi = Calculadora.dividir(numero, numero1);
-		System.out.println("La división es: " + divi);
+		Integer solucion = Calculadora.calcular(cadena, numero, numero1);
+		System.out.println("La solución es " + solucion);
 		sc.close();
 	}
 }
