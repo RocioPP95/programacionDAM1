27762de8ae27a14ff8e47f29f4bd4eaf8fcd5f53
Rocio Pacheco Ponce <rociopa1995@gmail.com>
2025-03-27 16:41:13 +0100

    .

---
+++ programacion3/src/ej57ListaPalabras/Diccionario.java
@@ -0,0 +1,18 @@
+package ej57ListaPalabras;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Diccionario {
+	private Map<String, List<String>> diccionario;
+
+	public Diccionario(Map<String, List<String>> diccionario) {
+		super();
+		this.diccionario = new HashMap<>();
+		
+	}
+	
+	
+
+}
